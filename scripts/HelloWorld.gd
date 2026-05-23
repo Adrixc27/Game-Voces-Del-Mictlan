6428ef3b183a18fccf7b416c4a8d30aee0b479fd
Adrixc27 <elcreeper97oficcial@gmail.com>
2026-05-22 22:30:16 -0600

    innit Commit
    ahora si el de verdad (creo)

---
+++ scripts/HelloWorld.gd
@@ -0,0 +1,13 @@
+extends Node3D
+
+func _ready():
+	var label = Label.new()
+	var label2 = Label.new()
+
+	label.text = "Hola Mundooooooooooo"
+	label.position = Vector2(20, 20)
+	add_child(label)
+
+	label2.text = "Hola Muchachos esta es una prueba, plantilla oficial del juego :D"
+	label2.position = Vector2(20, 60)
+	add_child(label2)
